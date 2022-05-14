fae4735b9d3589090f793d6c44c652924519a06b
thuyiya <thusitha.4t@gmail.com>
2022-05-14 15:15:08 +0530

    navigator setup

--- src/tests/jest-setup.js
+++ src/tests/jest-setup.js
@@ -1,9 +1,11 @@
-import 'react-native-gesture-handler/jestSetup';
+import "react-native-gesture-handler/jestSetup";
+import { jest } from "@jest/globals";
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
+jest.mock("react-native-reanimated", () => {
+  // eslint-disable-next-line no-undef
+  const Reanimated = require("react-native-reanimated/mock");
 
   // The mock for `call` immediately calls the callback which is incorrect
   // So we override it with a no-op
